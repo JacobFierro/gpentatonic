a15b6be974a59a4f2d08be07f7c838740de9aa04
Jacob Fierro <jacob.fierro@gmail.com>
2012-05-15 20:34:30 -0400

    added some color

--- js/script.js
+++ js/script.js
@@ -12,9 +12,15 @@ var sounds = [
 	'sounds/e.mp3'
 ];
 var buffers = [];
+var colors = {
+	g : '#B6E1E8',
+	a : '#D5E8B6',
+	b : '#E8D1B6',
+	d : '#D3C9DC',
+	e : '#F8C2CD'
+}
 
 function makeBuffers(list) {
-	console.log('list: ', list);
 	buffers.g = list[0];
 	buffers.a = list[1];
 	buffers.b = list[2];
@@ -22,10 +28,8 @@ function makeBuffers(list) {
 	buffers.e = list[4];
 }
 
-
-function finishedLoading(bufferList) {
-	makeBuffers(bufferList);
-	bindToKeys();
+function setStatus(msg) {
+	$('#alert').text(msg);
 }
 
 function bindToKeys() {
@@ -34,18 +38,23 @@ function bindToKeys() {
 		switch (key) {
 			case 83 :
 				playSound(buffers.g, 0);
+				setColor('g');
 				break;
 			case 68 :
 				playSound(buffers.a, 0);
+				setColor('a');
 				break;
 			case 70 :
 				playSound(buffers.b, 0);
+				setColor('b');
 				break;
 			case 74 :
 				playSound(buffers.d, 0);
+				setColor('d');
 				break;
 			case 75 :
 				playSound(buffers.e, 0);
+				setColor('e');
 				break;
 		}
 	});
@@ -58,6 +67,15 @@ function playSound(buffer, time) {
   source.noteOn(time);
 }
 
+function setColor (note) {
+	$('body').animate({ backgroundColor: colors[note] }, 100);
+}
+
+function finishedLoading(bufferList) {
+	makeBuffers(bufferList);
+	bindToKeys();
+	setStatus('ready');
+}
 
 function init() {
     context = new webkitAudioContext();
@@ -68,5 +86,4 @@ function init() {
 
 $(document).ready(function() {
 	init();
-	console.log('*hi');
 });
